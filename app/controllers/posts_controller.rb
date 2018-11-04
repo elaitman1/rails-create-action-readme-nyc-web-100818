e0afd29a4490bc0d20e2c2c60ba09ac79fc03c1a
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-11-04 15:11:43 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,21 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+    def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  # <%= form_tag posts_path do %>
+  #   <label>Post title:</label><br>
+  #   <%= text_field_tag :title %><br>
+  #
+  #   <label>Post Description</label><br>
+  #   <%= text_area_tag :description %><br>
+  #
+  #   <%= submit_tag "Submit Post" %>
+  # <% end %>
 end
